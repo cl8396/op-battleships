ae408dc76d0dc9a84e74fe13181edd42fedf9eed
Curtis <cl8396@gmail.com>
2023-12-03 12:44:43 +0000

    Add gameboard container component

---
+++ src/components/gameboards-container/gameboards-container.js
@@ -0,0 +1,44 @@
+import eventEmitter from '../../modules/event-emitter';
+import GameboardComponent from '../gameboard/gameboard';
+import { removeAllChildNodes } from '../../helper-functions';
+
+import './gameboards-container.css';
+
+class GameboardsContainer {
+  constructor(container) {
+    this.container = container;
+    this.element = this.render();
+
+    eventEmitter.on('playersCreated', (players) => {
+      this.populate(players);
+    });
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.classList.add('game__gameboards-container');
+    this.container.appendChild(element);
+    return element;
+  }
+
+  populate(players) {
+    for (const key in players) {
+      let player = players[key];
+      let container = document.createElement('div');
+      let header = document.createElement('header');
+      header.textContent = player.name;
+      container.appendChild(header);
+      let gameboard = new GameboardComponent(container, player.gameboard);
+      this.element.appendChild(container);
+    }
+  }
+
+  depopulate() {
+    removeAllChildNodes(this.element);
+    this.boards = [];
+  }
+
+  update() {}
+}
+
+export default GameboardsContainer;
