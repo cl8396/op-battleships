6634c2c461cc2ba1e85f6f268513019457a66383
Curtis <cl8396@gmail.com>
2023-12-04 10:02:55 +0000

    Add gameboard container to UI

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,13 +1,14 @@
 import eventEmitter from './event-emitter';
-import GameStatusDisplay from '../components/game-status';
+import GameStatusDisplay from '../components/game-status/game-status.js';
+import GameboardsContainer from '../components/gameboards-container/gameboards-container';
+import GameboardComponent from '../components/gameboard/gameboard';
 
 class UI {
   constructor() {
     this.container = document.getElementById('Battleships');
     this.gameStatusDisplay = new GameStatusDisplay(this.container);
+    this.gameboardsContainer = new GameboardsContainer(this.container);
   }
-
-  init() {}
 }
 
 const ui = new UI();
