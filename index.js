833e8a16d9561e327f7da8b59e7e03a600a116b7
Curtis <cl8396@gmail.com>
2023-12-09 12:49:28 +0000

    Remove testing code

--- index.js
+++ index.js
@@ -2,7 +2,3 @@ import './src/styles/main.css';
 
 import gameController from './src/modules/game-controller';
 import ui from './src/modules/ui';
-
-gameController.newGame();
-
-gameController.processTurn([1, 1]);
